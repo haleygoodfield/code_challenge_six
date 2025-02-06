312bf228e4ad1a8f58d28395059fae36e5c2f987
Haley Goodfield <haleygoodfield@usf.edu>
2025-02-06 12:28:40 -0500

    Task 5 - Currency Conversion

--- cc_6s.js
+++ cc_6s.js
@@ -47,3 +47,14 @@ function calculateSubcriptionCost(plan, months, discount = 0) {
 
 console.log(calculateSubscriptionCost("Basic", 6, 10)); // Expected output: "Total Cost: $50"
 console.log(calculateSubscriptionCost("Premium", 12, 0)); // Expected output: "Total Cost: $240"
+
+
+
+// Task 5: Returnign Values
+// Write a function convertCurrency(amount, exchangeRate) that returns the converted amount
+function convertCurrency(amount, exchangeRate) {
+    let convertedAmount = amount * exchangeRate;
+    return `Converted Amount: $${convertedAmount.toFixed(2)}`;
+};
+console.log(convertCurrency(100, 1.1)); // Expected output: "Converted Amount: $110.00")
+console.log(convertCurrency(250, 0.85)); // Expected output: "Converted Amount: $212.50")
